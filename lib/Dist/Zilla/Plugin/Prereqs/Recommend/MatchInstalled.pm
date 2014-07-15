d7ac64bfc550b6c389ee50d9520d053d8230d44f
Kent Fredric <kentfredric@gmail.com>
2014-07-16 03:55:24 +1200

    v0.002000
     [00 Major]
     - Various changes that may affect downstreams who work by hand.
     - Misc other packaging cleanups.
    
     [01 Tooling Changes]
     - Now uses EUMM instead of MB ( https://questhub.io/realm/perl/quest/5388cc12bbd0be5b2500011f )
     - Dependencies on "latest" toolchain parts dropped from "requires" to "recommends"
    
     [Dependencies::Stats]
     - Dependencies changed since 0.001000, see misc/*.deps* for details
     - build: -1 (recommends: -1)
     - configure: +1 -1 (recommends: +1 -1)
     - develop: +5 -5 (suggests: +2 -1)
     - test: (recommends: +1 ↑1)

--- lib/Dist/Zilla/Plugin/Prereqs/Recommend/MatchInstalled.pm
+++ lib/Dist/Zilla/Plugin/Prereqs/Recommend/MatchInstalled.pm
@@ -5,7 +5,7 @@ use utf8;
 
 package Dist::Zilla::Plugin::Prereqs::Recommend::MatchInstalled;
 
-our $VERSION = '0.002000';
+our $VERSION = '0.002001';
 
 # ABSTRACT: Advertise versions of things you have as soft dependencies
 
