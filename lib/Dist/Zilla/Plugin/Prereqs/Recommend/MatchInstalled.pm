8b1ac87e8663a742b02ab2f4f5f6d005fd9a1dd0
Kent Fredric <kentfredric@gmail.com>
2014-08-16 07:49:33 +1200

    Build results of 2c7b316 (on master)

--- lib/Dist/Zilla/Plugin/Prereqs/Recommend/MatchInstalled.pm
+++ lib/Dist/Zilla/Plugin/Prereqs/Recommend/MatchInstalled.pm
@@ -11,46 +11,6 @@ our $VERSION = '0.002001';
 
 our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 use Moose qw( with has around );
 use MooseX::Types::Moose qw( HashRef ArrayRef Str );
 with 'Dist::Zilla::Role::PrereqSource';
