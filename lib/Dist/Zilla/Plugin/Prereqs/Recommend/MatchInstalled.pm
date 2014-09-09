55691785918622ab28861e4cb489c9a3a534159f
Kent Fredric <kentfredric@gmail.com>
2014-09-10 02:17:42 +1200

    v0.003001
     [Bugfix]
     - Fix PosixLower not being a thing before 5.10.1

--- lib/Dist/Zilla/Plugin/Prereqs/Recommend/MatchInstalled.pm
+++ lib/Dist/Zilla/Plugin/Prereqs/Recommend/MatchInstalled.pm
@@ -5,7 +5,7 @@ use utf8;
 
 package Dist::Zilla::Plugin::Prereqs::Recommend::MatchInstalled;
 
-our $VERSION = '0.003001';
+our $VERSION = '0.003002';
 
 # ABSTRACT: Advertise versions of things you have as soft dependencies
 
