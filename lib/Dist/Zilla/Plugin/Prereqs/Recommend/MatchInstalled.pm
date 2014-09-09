bde31ca108fff83720642ecfa871c056f5fcadc7
Kent Fredric <kentfredric@gmail.com>
2014-09-10 02:17:57 +1200

    Build results of 5569178 (on master)

--- lib/Dist/Zilla/Plugin/Prereqs/Recommend/MatchInstalled.pm
+++ lib/Dist/Zilla/Plugin/Prereqs/Recommend/MatchInstalled.pm
@@ -5,7 +5,7 @@ use utf8;
 
 package Dist::Zilla::Plugin::Prereqs::Recommend::MatchInstalled;
 
-our $VERSION = '0.003001';
+our $VERSION = '0.003002';
 
 # ABSTRACT: Advertise versions of things you have as soft dependencies
 
@@ -293,7 +293,7 @@ Dist::Zilla::Plugin::Prereqs::Recommend::MatchInstalled - Advertise versions of 
 
 =head1 VERSION
 
-version 0.003001
+version 0.003002
 
 =head1 SYNOPSIS
 
