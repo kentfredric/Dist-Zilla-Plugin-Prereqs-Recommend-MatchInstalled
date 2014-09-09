037697195d07ef10d331f76dd7f9068a28384a8f
Kent Fredric <kentfredric@gmail.com>
2014-09-10 02:16:46 +1200

    Fix PosixLower not being a thing before 5.10 + - Fix PosixLower not being a thing before 5.10.1

--- lib/Dist/Zilla/Plugin/Prereqs/Recommend/MatchInstalled.pm
+++ lib/Dist/Zilla/Plugin/Prereqs/Recommend/MatchInstalled.pm
@@ -142,13 +142,15 @@ has '_applyto_map_hash' => (
 
 # _Pulp__5010_qr_m_propagate_properly
 ## no critic (Compatibility::PerlMinimumVersionAndWhy)
-my $word  = qr/\p{PosixLower}+/msx;
-my $combo = qr/${word}[.]${word}/msx;
+my $re_phase    = qr/configure|build|runtime|test|develop/msx;
+my $re_relation = qr/requires|recommends|suggests|conflicts/msx;
+
+my $combo = qr/(?:$re_phase)[.](?:$re_relation)/msx;
 
 sub _parse_map_token {
   my ( $self, $token ) = @_;
   my ( $phase, $relation );
-  if ( ( $phase, $relation ) = $token =~ /\A(${word})[.](${word})/msx ) {
+  if ( ( $phase, $relation ) = $token =~ /\A($re_phase)[.]($re_relation)/msx ) {
     return {
       phase    => $phase,
       relation => $relation,
